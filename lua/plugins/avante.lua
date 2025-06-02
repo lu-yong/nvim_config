1e31d67223a6948bd50e501a6fd553b8e6d65fc1
luyong <skyplan454@gmail.com>
2025-06-02 23:41:10 +0800

    chore(avante): delete unnecessary dependency

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -88,7 +88,6 @@ return {
     --- The below dependencies are optional,
     "echasnovski/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "echasnovski/mini.icons", -- or nvim-tree/nvim-web-devicons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
