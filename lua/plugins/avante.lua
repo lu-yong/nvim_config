1f4e8082e4913439b8dd10077b670680243831f9
luyong <skyplan454@gmail.com>
2025-06-08 22:59:49 +0800

    chore(avante): remove unneeded dependency and imporve load speed

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -81,15 +81,12 @@ return {
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "stevearc/dressing.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "echasnovski/mini.icons", -- or nvim-tree/nvim-web-devicons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    --"echasnovski/mini.icons", -- or nvim-tree/nvim-web-devicons
+    --"zbirenbaum/copilot.lua", -- for providers='copilot'
     "ravitemer/mcphub.nvim", -- for mcphub server
     {
       -- support for image pasting
