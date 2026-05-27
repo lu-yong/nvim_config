2895504dc87647b583e53a3d5f05964129b3d8b4
luyong <luyong@nationalchip.com>
2026-05-27 09:22:24 +0800

    chore(sidekick): update keybinding

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -9,6 +9,16 @@ return {
   },
   {
     "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        win = {
+          keys = {
+            buffers       = { "<A-b>", "buffers"   , mode = "nt", desc = "open buffer picker" },
+            files         = { "<A-f>", "files"     , mode = "nt", desc = "open file picker" },
+          },
+        },
+      },
+    },
     keys = {
       { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n", "v" }, expr = true },
     },
